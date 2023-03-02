f77e421ad186930c7de5e031ce9a79952e92e2de
Oleksandr <alex.vovchok@gmail.com>
2023-03-02 22:29:55 +0200

    start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    box: document.querySelector('.country-info'),
+};
+
+// https://restcountries.com/v3.1/name/{name}
+// https://restcountries.com/v3.1/name/peru
+
+function fetchCountries(name) {
+    
+}
